2cb916e34978339047f060dd765ae2b361403a58
uri <uriyhoo123@gmail.com>
2025-11-28 20:54:12 +0200

    Add explicit return statements in withdrawal and fee application methods

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,12 +1,17 @@
 import java.math.BigDecimal;
 
 public class BankApp {
-    public static void main(String[] args) throws InsufficientFundsException {
-        Bank tauBank = new Bank();
-        SavingsAccount saving = new SavingsAccount("67", "Uri", new BigDecimal("1000000000.00"));
-        tauBank.addAccount(saving);
-        CheckingAccount checking = new CheckingAccount("69", "Jony", new BigDecimal("99999999999999.00"));
-        tauBank.addAccount(checking);
-        tauBank.applyMonthlyFees();
+    public static void main(String[] args) {
+        try {
+            Bank tauBank = new Bank();
+            SavingsAccount saving = new SavingsAccount("67", "Uri", new BigDecimal("1000000000.00"));
+            tauBank.addAccount(saving);
+            CheckingAccount checking = new CheckingAccount("69", "Jony", new BigDecimal("99999999999999.00"));
+            tauBank.addAccount(checking);
+            tauBank.applyMonthlyFees();
+        } catch (InsufficientFundsException e) {
+            System.err.println("--- FATAL BANKING ERROR ---");
+            System.err.println("Transaction Halted: " + e.getMessage());
+        }
     }
 }
